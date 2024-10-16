aec66b1da4883d6b1624176d76ae94897ffc666c
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2024-10-16 10:10:12 +0100

    feat: update landing page bike

--- components/home/hero/index.vue
+++ components/home/hero/index.vue
@@ -4,7 +4,7 @@ import BentoCustomers from "./components/bento-customers.vue";
 </script>
 
 <template>
-  <section class="pt-16" id="home">
+  <section class="pt-16 overflow-hidden" id="home">
     <h1
       class="space-grotesk font-bold w-[300px] md:w-[400px] text-center text-[32px] leading-[40px] md:text-[48px] md:leading-[50px] text-[#1E1E1E] mx-auto"
     >
@@ -13,7 +13,9 @@ import BentoCustomers from "./components/bento-customers.vue";
       Partner
     </h1>
 
-    <p class="mx-auto md:w-[600px] text-center mt-5 md:text-lg text-[#737373]">
+    <p
+      class="mx-auto w-[400px] md:w-[600px] text-center mt-5 md:text-lg text-[#737373]"
+    >
       Experience the future of logistics with Zipkod -- delivering excellence,
       one mile at a time.
     </p>
@@ -29,14 +31,16 @@ import BentoCustomers from "./components/bento-customers.vue";
       class="flex flex-col md:flex-row gap-x-4 gap-y-5 w-[90%] xl:w-[1200px] 2xl:w-[1344px] mx-auto mt-14"
     >
       <div
-        class="w-full md:w-[55%] lg:w-[60%] h-[300px] lg:h-[350px] xl:h-[450px] 2xl:h-[500px] bg-gray-100 rounded-[24px]"
+        class="w-full md:w-[55%] lg:w-[60%] h-[300px] lg:h-[350px] xl:h-[450px] 2xl:h-[500px] bg-gray-100 rounded-[24px] relative"
       >
-        <div class="w-full h-full relative">
+        <div class="w-[108%] h-[108%] relative">
           <img
-            src="/images/bike.png"
-            class="w-full h-full absolute z-10 object-cover"
+            src="/images/hero/bike-desktop.png"
+            class="w-full h-full absolute z-20 left-5 bottom-7 md:bottom-12"
           />
         </div>
+
+        <img src="/images/hero/shadow.png" class="absolute bottom-0" />
       </div>
 
       <!-- gap-y-3 lg:gap-y-5 -->
@@ -45,7 +49,7 @@ import BentoCustomers from "./components/bento-customers.vue";
         class="w-full md:w-[45%] lg:w-[40%] flex flex-col justify-between gap-y-3 lg:gap-y-0"
       >
         <div
-          class="w-full h-[190px] lg:h-[245px] xl:h-[330px] 2xl:h-[370px] bg-black rounded-3xl md:rounded-2xl lg:rounded-3xl relative"
+          class="w-full h-[260px] md:h-[190px] lg:h-[245px] xl:h-[330px] 2xl:h-[370px] bg-black rounded-3xl md:rounded-2xl lg:rounded-3xl relative"
         >
           <client-only>
             <Vue3Lottie
