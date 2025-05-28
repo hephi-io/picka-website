959c1481089acf226563bb8dc8f24b3dd71ba196
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2025-05-28 12:23:18 +0100

    chore: add shadcn to the project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,25 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
   css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss() as any],
+  },
   app: {
     head: {
       charset: "utf-8",
