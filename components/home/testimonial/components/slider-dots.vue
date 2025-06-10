b1c61e19cc1a5e94ed241cd802313904c66c9c03
Tochi-Ezeani <ezeanibarry1@gmail.com>
2025-06-10 14:40:52 +0100

    feat: added the testimonial section for the picka landing page

--- components/home/testimonial/components/slider-dots.vue
+++ components/home/testimonial/components/slider-dots.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-x-[9px] items-center">
-    <div class="w-2 h-2 bg-white rounded-full"></div>
-    <div class="w-2 h-2 bg-white opacity-20 rounded-full"></div>
+    <div :class="`w-2 h-2 rounded-full ${currentSlide === 1 ? 'bg-white' : 'bg-white opacity-20'}`"></div>
+    <div :class="`w-2 h-2 rounded-full ${currentSlide === 2 ? 'bg-white' : 'bg-white opacity-20'}`"></div>
   </div>
 </template>
 
